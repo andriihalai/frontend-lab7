4edca16e880161750d4dd71bdcafa5212d0cfbbd
Andrii Halai <halaiandriy@gmail.com>
2024-11-17 20:13:43 +0200

    Fix images loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,22 @@ import Header from './components/Header';
 import Content from './components/Content';
 import ImagesBox from './components/ImagesBox';
 import GoodsGallery from './components/GoodsGallery';
+import pearImage from './assets/pear.png';
+import grapeImage from './assets/grape.png';
+import peachImage from './assets/peach.png';
+import appleImage from './assets/apple.png';
+import cherryImage from './assets/cherry.png';
+import kiwiImage from './assets/kiwi.png';
+import orangeImage from './assets/orange.png';
 
 const goods = [
-  {
-    imageUrl: '/src/assets/pear.png',
-    itemName: 'Pear',
-    cost: 40,
-  },
-  {
-    imageUrl: '/src/assets/grape.png',
-    itemName: 'Grape',
-    cost: 100,
-  },
-  {
-    imageUrl: '/src/assets/peach.png',
-    itemName: 'Peach',
-    cost: 50,
-  },
-  {
-    imageUrl: '/src/assets/apple.png',
-    itemName: 'Apple',
-    cost: 30,
-  },
-  {
-    imageUrl: '/src/assets/cherry.png',
-    itemName: 'Cherry',
-    cost: 100,
-  },
-  {
-    imageUrl: '/src/assets/kiwi.png',
-    itemName: 'Kiwi',
-    cost: 88,
-  },
-  {
-    imageUrl: '/src/assets/orange.png',
-    itemName: 'Orange',
-    cost: 79,
-  },
+  { imageUrl: pearImage, itemName: 'Pear', cost: 40 },
+  { imageUrl: grapeImage, itemName: 'Grape', cost: 100 },
+  { imageUrl: peachImage, itemName: 'Peach', cost: 50 },
+  { imageUrl: appleImage, itemName: 'Apple', cost: 30 },
+  { imageUrl: cherryImage, itemName: 'Cherry', cost: 100 },
+  { imageUrl: kiwiImage, itemName: 'Kiwi', cost: 88 },
+  { imageUrl: orangeImage, itemName: 'Orange', cost: 79 },
 ];
 
 export default function App() {
